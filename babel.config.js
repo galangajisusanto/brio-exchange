2a9dd67e2a21c01722667262c5a473ed0c93bbc2
Galang Aji Susanto <galang.aji@mekari.com>
2025-06-09 02:43:14 +0700

    [Improvement] simplify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: [
-            ['babel-preset-expo', { jsxImportSource: 'react' }],
-            ['@babel/preset-env', { targets: { node: 'current' } }],
-            ['@babel/preset-react', { runtime: 'automatic' }]
-        ],
+        presets: ['babel-preset-expo'],
         plugins: [],
     };
 };
